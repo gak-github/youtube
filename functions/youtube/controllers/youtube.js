afe8389aefb62d7d59a0d6ef4f0e205911b2c086
Asokumar Gurusamy <Asokumar Gurusamy>
2020-04-27 10:01:19 -0700

    responsive issue fix, header color change and reading from local data to limit the quota

--- functions/youtube/controllers/youtube.js
+++ functions/youtube/controllers/youtube.js
@@ -1,22 +1,33 @@
 const axios = require('axios');
 const _get = require('lodash').get
+const ytData = require('../config/yt-data');
 // const ytSearch = require('youtube-search');
 
 // @desc get youtube videos for the tiven search term
 // @route GET /api/v1/youtube
 // @access Public
 exports.getVideos = async (req, res, next) => {
+  let testData;
   const KEY = process.env.YT_KEY;
+  // if it is test environment then don't hit actual API to save the daily data quota limit
+  if (!KEY) {
+    return res.status(200).json({
+      success: true,
+      data: ytData
+    });
+  }
+
   const youtubeApi = axios.create({
     baseURL: "https://www.googleapis.com/youtube/v3"
   });
+
   const term = req.query.search;
   try {
     const response = await youtubeApi.get("/search", {
         params: {
           q: term,
           part: "snippet",
-          maxResults: 6,
+          maxResults: 5,
           type: "video",
           key: KEY
         }
@@ -28,15 +39,16 @@ exports.getVideos = async (req, res, next) => {
     //   part: snippet
     // };
 
-    // const response = await ytSearch(term, options);
+    // const response = await ytSearch(term, options); // this can be used for debugging purpose instead of using axios
     return res.status(200).json({
       success: true,
       data: _get(response, 'data.items') || []
     });
   } catch(error) {
-    return res.send(400).json({
-      success: false,
-      error: error.message,
+    // TODO: if headers already sent
+    return res.status(200).json({ // send test data in case of error
+      success: true,
+      data: ytData
     });
   }
 };
