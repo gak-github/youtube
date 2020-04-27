6d06c04c573bc1d24bbfd2138320e20297b5cfcf
Asokumar Gurusamy <Asokumar Gurusamy>
2020-04-26 19:50:53 -0700

    debugging the forbidden error due to quota issue

--- functions/youtube/controllers/youtube.js
+++ functions/youtube/controllers/youtube.js
@@ -1,10 +1,12 @@
 const axios = require('axios');
 const _get = require('lodash').get
+// const ytSearch = require('youtube-search');
+
 // @desc get youtube videos for the tiven search term
 // @route GET /api/v1/youtube
 // @access Public
 exports.getVideos = async (req, res, next) => {
-  const KEY = process.env.YT_KEY || "AIzaSyAiWS2ZvLEtdRDTQjw4KwFOqOuY5qRO0fo";
+  const KEY = process.env.YT_KEY;
   const youtubeApi = axios.create({
     baseURL: "https://www.googleapis.com/youtube/v3"
   });
@@ -19,12 +21,20 @@ exports.getVideos = async (req, res, next) => {
           key: KEY
         }
     });
+  
+    // let options = {
+    //   maxResults: 5,
+    //   key: KEY,
+    //   part: snippet
+    // };
+
+    // const response = await ytSearch(term, options);
     return res.status(200).json({
       success: true,
       data: _get(response, 'data.items') || []
     });
   } catch(error) {
-    return res.send(500).json({
+    return res.send(400).json({
       success: false,
       error: error.message,
     });
