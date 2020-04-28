7fee7393dc09fb01b6de3dce5a5c5603697bd82a
Asokumar Gurusamy <Asokumar Gurusamy>
2020-04-28 10:13:28 -0700

    randomly selecting the default play item

--- src/context/AppReducer.js
+++ src/context/AppReducer.js
@@ -3,13 +3,15 @@ const _get = require('lodash').get;
 export default (state, action) => {
   switch (action.type) {
     case "GET_VIDEOS":
+      const listSize = _get(action, 'payload.videos').length;
+      const random = Math.floor(Math.random() * Math.floor(listSize));
       return {
         ...state,
         loading: true,
         videos: _get(action, 'payload.videos'),
         term: '',
         isTestData: _get(action, 'payload.isTestData'),
-        selectedVideo: _get(action, 'payload.videos').length > 0 ? action.payload.videos[0] : ''
+        selectedVideo: listSize > 0 ? action.payload.videos[random] : ''
       }
     case "SET_SELECTED_VIDEO": 
       return {
