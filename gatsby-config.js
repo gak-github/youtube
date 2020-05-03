7040f97b48e58cf44972844c11ecb2c5fabb99af
Asokumar Gurusamy <Asokumar Gurusamy>
2020-05-03 09:07:10 -0700

    fixing scss partial import issue

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,12 +1,12 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Asokumar Gurusamy`,
+    description: `A full stack web application using youtube data api and built on gatsby using React Hooks and Context API`,
+    author: `Asokumar Gurusamy`,
+    keywords: ['agurusamy', 'Ashok Kumar', 'Asokumar Gurusamy', 'Ashok G', 'Ashok Gurusamy', 'serverless netlify', 'netlify-lambda']
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
-    `gatsby-plugin-sass`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -14,23 +14,20 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
+    `gatsby-plugin-sass`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
-    `gatsby-plugin-svg-sprite`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `agurusamy-youtube-app`,
+        short_name: `agurusamy-yt`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
         icon: `src/images/leaf.png`, // This path is relative to the root of the site.
       },
-    },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    }
   ],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,11 +4,13 @@ import Header from "../components/Header";
 import Main from "../components/Main";
 import Layout from "../components/Layout"
 import { GlobalProvider } from "../context/GlobalState";
+import SEO from '../components/SEO';
 
 function IndexPage() {
   return (
     <GlobalProvider>
       <Layout>
+        <SEO title='Home' />
         <Header />
         <Main />
       </Layout>
